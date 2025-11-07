124f636408ea17ad05f75a746e46eb33b0d27932
gaga190608 <gabrielle.calazaans@gmail.com>
2025-11-07 19:12:00 -0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,7 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
- 
-  darkMode: 'class', 
-
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
+  darkMode: 'class',
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: { extend: {} },
   plugins: [],
 }
