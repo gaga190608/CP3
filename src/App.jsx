d1eeb9b858376eb3eb538178b753eeabfeac6560
gaga190608 <gabrielle.calazaans@gmail.com>
2025-11-07 18:32:50 -0300

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./sections/Hero";
-import Projects from "./section/Projects";
+import Projects from "./sections/Projects";
 import Skills from "./sections/Skills";
 import About from "./sections/About";
 import Contact from "./sections/Contact";
